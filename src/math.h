dae78c1cebb44a9791d3d8c269b489a789966a81
Elliot Lu <ellilu1191@gmail.com>
2026-01-09 23:24:13 -0500

    Minor updates

--- src/math.h
+++ src/math.h
@@ -6,33 +6,7 @@
 
 #define _USE_MATH_DEFINES
 
-template <typename T>
-std::vector<T> operator+ (const std::vector<T>& zs, const std::vector<T>& ws) {
-    std::vector<T> sum;
-    for (size_t i = 0; i < zs.size(); ++i)
-        sum.push_back(zs[i] + ws[i]);
-    return sum;
-}
-
-std::ostream& operator<< (std::ostream& os, const vec3d& X) {
-    os << X[0] << " " << X[1] << " " << X[2];
-    return os;
-}
-
-std::istream& operator>>(std::istream& is, vec3d& X) {
-    double x, y, z;
-    if (is >> x >> y >> z)
-        X = vec3d{ x, y, z };
-    return is;
-}
-
-std::array<bool,3> operator> (const vec3d& x, const vec3d& y) {
-    std::array<bool,3> bools{ x[0] > y[0], x[1] > y[1], x[2] > y[2] };
-    return bools;
-}
-
 namespace Math {
-
     inline size_t bools2Idx(const std::array<bool, 3>& x) noexcept {
         return x[0] + 2 * x[1] + 4 * x[2];
     }
--- src/types.h
+++ src/types.h
@@ -19,3 +19,28 @@ using matXcd = Eigen::MatrixXcd;
 constexpr cmplx iu(0, 1);
 const double PI = std::acos(-1.0);
 const vec3d zeroVec = vec3d::Zero();
+
+template <typename T>
+std::vector<T> operator+ (const std::vector<T>& zs, const std::vector<T>& ws) {
+    std::vector<T> sum;
+    for (size_t i = 0; i < zs.size(); ++i)
+        sum.push_back(zs[i] + ws[i]);
+    return sum;
+}
+
+std::ostream& operator<< (std::ostream& os, const vec3d& X) {
+    os << X[0] << " " << X[1] << " " << X[2];
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, vec3d& X) {
+    double x, y, z;
+    if (is >> x >> y >> z)
+        X = vec3d{ x, y, z };
+    return is;
+}
+
+std::array<bool, 3> operator> (const vec3d& x, const vec3d& y) {
+    std::array<bool, 3> bools{ x[0] > y[0], x[1] > y[1], x[2] > y[2] };
+    return bools;
+}
